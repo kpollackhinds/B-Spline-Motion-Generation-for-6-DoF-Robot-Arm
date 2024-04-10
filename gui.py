1b0a179522436e6123ee0ab8cc9a44ec225d748a
kpollackhinds <kahlil.pollack-hinds@stonybrook.edu>
2024-04-10 14:51:23 -0400

    added quaternion math

--- gui.py
+++ gui.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 from tkinter import filedialog
 import numpy as np
+from numpy import deg2rad as rad
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-
+import quaternion 
+from dual_quaternions import DualQuaternion
 # Assume `parse_pose` and `run_motion` are defined in helper_functions
 from helper_functions import parse_pose, run_motion
 
@@ -12,7 +14,22 @@ def open_file():
     file = filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("Text files", "*.txt*"), ("all files", "*.*")))
     if file:
         selected_coords = parse_pose(file)
-        update_listbox()
+        update_motion()
+
+def update_motion():
+    update_listbox()
+
+    global dual_quaternions
+    dual_quaternion = []
+    if selected_coords:
+        for coord in selected_coords:
+            temp_quaternion = quaternion.from_euler_angles([rad(c) for c in coord[3:6]])
+            print(temp_quaternion.type(), temp_quaternion)
+            temp_dq = DualQuaternion.from_quat_pose_array(temp_quaternion.tolist().extend(coord[0:3]))
+            dual_quaternion.append(temp_dq)
+    print(dual_quaternion)
+
+
 
 def update_listbox():
     listbox.delete(0, tk.END)
@@ -52,7 +69,7 @@ right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 # Control buttons
 button_frame = tk.Frame(root)
 tk.Button(button_frame, text="Browse", command=open_file).pack(side=tk.LEFT, padx=10)
-tk.Button(button_frame, text="Run Motion", command=run_motion).pack(side=tk.LEFT, padx=10)
+tk.Button(button_frame, text="Run Motion", command=update_motion).pack(side=tk.LEFT, padx=10)
 button_frame.pack(fill=tk.X)
 
 root.mainloop()
